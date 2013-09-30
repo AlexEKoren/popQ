afdb0a2aae26f1ebe1b6cb72573471ab1ada8f85
Alex Koren <alexkoren@MAC04011-CUE-Laptop.local>
2013-09-30 12:32:46 -0400

    Initial Commit

---
+++ popQ/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  popQ
+//
+//  Created by Alex Koren on 9/30/13.
+//  Copyright (c) 2013 AKApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
